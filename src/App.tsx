2c1736860d63738517df5522e96ce590d52b4fbf
leemirae <416homin@daum.net>
2024-02-11 17:52:57 +0900

    'ci-test7'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ci test</p>
+        <p>ci test8</p>
         <a
           className="App-link"
           href="https://reactjs.org"
