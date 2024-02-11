36119882ef5b3dea6bc3ea63234e48e33d870a07
leemirae <416homin@daum.net>
2024-02-11 18:35:34 +0900

    'runner2'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>runner</p>
+        <p>runner2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
